b909774c3b4758b79f23f8ed518e25d1a769916b
GolfForecast <Tammie.winward@gmail.com>
2025-08-08 14:16:11 +1000

    Create layout.tsx

---
+++ app/app/app/layout.tsx
@@ -0,0 +1,6 @@
+<nav className="text-sm flex gap-4">
+  <a href="/" className="hover:underline">Feed</a>
+  <a href="/courses" className="hover:underline">Courses</a>   {/* ← add this */}
+  <a href="/guess" className="hover:underline">Guess This Course</a>
+  <a href="/mental" className="hover:underline">Mental Fitness</a>
+</nav>
